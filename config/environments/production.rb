89eef34e280fcd6d7a516b0be8633e2aa56c9917
Phil Nguyen <pnguyen06@gmail.com>
2014-09-24 18:01:26 -0400

    Devise work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Hotdogsormelany::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Devise
+  # Set to actual hostname
+  config.action_mailer.default_url_options = { :host => 'http://hotdogsormelany.com' }
 end
