75e821a9949c49cfe709152a40187b522a01925e
Phil Nguyen <pnguyen06@gmail.com>
2014-09-24 12:59:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hotdogsormelany::Application.config.secret_key_base = '9a8f08a5df3c1d07b9fbfc8acdfd0414707ae4e7918972a86272034e628ccd611201e63fe16fad8cacc02634f2429f9da89d133d9d8ce56a8cf7131e83875832'
