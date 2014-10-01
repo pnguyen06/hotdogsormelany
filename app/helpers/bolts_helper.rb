23430bfda3a03a16c8349afa52c37050359f1dd4
Phil Nguyen <pnguyen06@gmail.com>
2014-10-01 13:14:12 -0400

    Time function fix

--- app/helpers/bolts_helper.rb
+++ app/helpers/bolts_helper.rb
@@ -2,13 +2,16 @@ module BoltsHelper
 	def short_age_string(time)
     diff = Time.now - time #value is seconds (float)
     diff = diff.abs.to_i
-    if diff >= 604800 #seconds in a week
-      weeks = diff/604800
-      return "#{weeks}#{"+" if weeks >= 999}W ago"
+    if diff >= 259200 #seconds in 3 days
+      return time.in_time_zone("Eastern Time (US & Canada)").strftime("%b %d")
+    elsif diff > 86400 #seconds in a day
+      return "#{diff/86400}d ago"
     elsif diff > 3600 #seconds in an hour
-      return "#{diff/3600}H ago"
+      return "#{diff/3600}h ago"
+    elsif diff > 60 #seconds in a minute
+      return "#{diff/60}m ago"
     else
-      return "#{diff/60}#{minutes}M ago"
+      return "#{diff}s ago"
     end
   end
 end
