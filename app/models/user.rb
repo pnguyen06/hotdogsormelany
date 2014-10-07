96e0d63702f8cac6f9cad4bf43035b78e78a2074
Phil Nguyen <pnguyen06@gmail.com>
2014-10-07 08:58:44 -0400

    default pics

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :team, presence: true
 
-  has_attached_file :avatar, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "missing.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
