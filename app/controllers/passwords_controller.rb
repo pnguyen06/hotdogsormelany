a031168b8f433f71c34417c0abeb99bdd65230ea
Phil Nguyen <pnguyen06@gmail.com>
2014-10-17 17:06:29 -0400

    passwords controller

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,5 +1,9 @@
 class PasswordsController < Devise::PasswordsController
   protected
+	def after_resetting_password_path_for(resource)
+    new_user_session_path
+   end
+
   def after_sending_reset_password_instructions_path_for(resource_name)
     new_user_session_path
   end
